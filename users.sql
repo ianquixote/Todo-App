ccadc3ddb5a339af22fd66eabfe89030bf9a0192
Ian Austin <ianaustin2560@gmail.com>
2021-11-26 21:00:18 -0800

    Updated users.sql

--- users.sql
+++ users.sql
@@ -2,3 +2,5 @@ INSERT INTO users (username, password)
   VALUES ('admin', '$2b$10$uYnqg3py6QjDQdTtV9Aozepm1aKtoExQOI5eyipzg4cOtCIodMVMG'),
          ('developer', '$2b$10$y3bitoHTZCglmI51PfUh9ehbdppNeHEXD8Ec973vCImx9lM6Wsht.'),
          ('somebody', 'knock-knock');
+
+--moved location
